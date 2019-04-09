9d7fd921651c53f07fd6f039636dc8898cbdb6f9
KAZUKI OKAYAMA <okymkzk0816@gmail.com>
2019-04-09 17:43:13 +0900

    first commit

---
+++ config/application.rb
@@ -0,0 +1,16 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+Bundler.require(*Rails.groups)
+
+module WeatherchanExtend
+  class Application < Rails::Application
+    config.load_defaults 5.2
+    config.generators do |g|
+      g.javascripts false
+      g.helper false
+      g.test_framework false
+    end
+  end
+end
